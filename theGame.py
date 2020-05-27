a3bb4907450eb5f2312a09a5332f43acda39f922
ssamko <teacher.ssamko@gmail.com>
2020-05-28 08:54:07 +0900

    end turn condition

--- theGame.py
+++ theGame.py
@@ -11,7 +11,6 @@ class TheGame:
         self.set_cards()
         self.handout()
         self.set_table()
-        self.cards_left = len(self.cards)
         self.drawed = 0
         self.turn_over = False
         self.turn_do()
@@ -42,37 +41,73 @@ class TheGame:
         self.asc2 = 1
         self.desc1 = 99
         self.desc2 = 99
-        self.table_status = f'(1)asc1:  {self.asc1:2} (2)asc2:  {self.asc2:2} \n(3)desc1: {self.desc1:2} (4)desc2: {self.desc2:2}'
-        print(self.table_status)
+        self.status_message()
 
     def turn_do(self):
+        self.turn_message()
+
+        while not self.turn_over:
+            if self.drawed < 2:
+                self.selected = int(
+                    input('Select a card(put the card number): '))
+            else:
+                self.selected = int(
+                    input("Select a card(put the card number) or end turn with '0': "))
+            print('*'*50)
+            if self.selected in self.playerA:
+                self.card_index = self.playerA.index(self.selected)
+                self.now_card = self.playerA.pop(self.card_index)
+                self.card_at = self.card_dest()
+                self.put_card_on(self.card_at)
+
+            # End turn phase
+            elif self.selected == 0 and self.drawed >= 2:
+                print('Turn finished')
+                self.drawed = 0
+                self.turn_over = True
+                break
+            else:
+                print('**Put the number correctly**')
+
+    def turn_message(self):
         print('*'*50)
         print('Turn of Player A')
         print('*'*50)
         print(self.playerA)
         print()
+        self.status_message()
+
+    def status_message(self):
+        self.cards_left = len(self.cards)
+        print(f'{self.cards_left} cards left ')
+        self.table_status = f'(1)asc1:  {self.asc1:2} (2)asc2:  {self.asc2:2} \n(3)desc1: {self.desc1:2} (4)desc2: {self.desc2:2}'
         print(self.table_status)
-
-        while not self.turn_over:
-            self.selected = int(input('Select a card(put the card number): '))
-            if self.selected in self.playerA:
-                self.card_index = self.playerA.index(self.selected)
-                self.now_card = self.playerA.pop(self.card_index)
-                print(f'Where do you want to put {self.now_card} on?')
-                print(self.table_status)
-                self.card_at = self.card_dest()
-
-                break
-            else:
-                print('**Put the number correctly**')
 
     def card_dest(self):
         while True:
+            print(f'Where do you want to put {self.now_card} on?')
+            print('*'*50)
+            print(self.table_status)
             self.dest = int(input('Type the number: '))
+            print('*'*50)
             if self.dest > 4 or self.dest < 1:
-                print('Type right line number(1 ~ 4)')
+                print('**Type right line number(1 ~ 4)**')
                 continue
             return self.dest
+
+    def put_card_on(self, dest):
+        if dest == 1:
+            self.asc1 = self.now_card
+        elif dest == 2:
+            self.asc2 = self.now_card
+        elif dest == 3:
+            self.desc1 = self.now_card
+        elif dest == 4:
+            self.desc2 = self.now_card
+
+        self.now_card = 0
+        self.drawed += 1
+        self.status_message()
 
 
 TheGame()
