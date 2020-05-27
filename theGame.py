8490ba60e0faa0ef25d56c3609edb0a8eb84631b
ssamko <teacher.ssamko@gmail.com>
2020-05-28 07:59:33 +0900

    card dest()

--- theGame.py
+++ theGame.py
@@ -19,6 +19,8 @@ holds = 7
 for i in range(holds):
     playerA.append(cards.pop())
     playerB.append(cards.pop())
+playerA.sort()
+playerB.sort()
 print(playerA)
 print(playerB)
 
@@ -28,7 +30,41 @@ asc1 = 1
 asc2 = 1
 desc1 = 99
 desc2 = 99
-table = f'asc1:  {asc1:2} asc2:  {asc2:2} \ndesc1: {desc1:2} desc2: {desc2:2}'
+table = f'(1)asc1:  {asc1:2} (2)asc2:  {asc2:2} \n(3)desc1: {desc1:2} (4)desc2: {desc2:2}'
 print(table)
 
+# Method for playing
+
+
+def card_dest():
+    while True:
+        dest = int(input('Type the number'))
+        if dest > 4 or dest < 1:
+            print('Type right line number(1 ~ 4): ')
+            continue
+        return dest
+
+
 # Select Cards
+cards_left = len(cards)
+drawed = 0
+turnover = False
+print('*'*50)
+print('Turn of Player A')
+print('*'*50)
+print(playerA)
+print()
+print(table)
+
+while not turnover:
+    selected = int(input('Select a card(put the card number): '))
+    if selected in playerA:
+        card_index = playerA.index(selected)
+        now_card = playerA.pop(card_index)
+        print(f'Where do you want to put {now_card} on?')
+        print(table)
+        card_dest = card_dest()
+
+        break
+    else:
+        print('**Put the number correctly**')
