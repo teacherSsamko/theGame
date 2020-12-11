8edbc7a7c772bbb2cf6f5e5c57d6034d14c63306
ssamko <ssamko@gdflab.com>
2020-12-11 09:07:58 +0900

    docstring

--- lines.py
+++ lines.py
@@ -1,10 +1,14 @@
 class Line:
-    def __init__(self, asc):
-        """
-        asc -- ascend or descend line
+    def __init__(self, asc=True):
+        """init line asc or desc
+
+        Parameters:
+        asc -- ascending or descding order. (default = True)
+
         """
         self.stack = []
         self.asc = asc
 
     def stack_on(self, n):
         self.stack.append(n)
+
