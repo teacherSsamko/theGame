2ee4c2d96a47343c77f88fef3146475aa82d09c6
ssamko <teacher.ssamko@gmail.com>
2020-12-12 00:18:05 +0900

    Asc Desc Line

--- lines.py
+++ lines.py
@@ -1,5 +1,5 @@
 class Line:
-    def __init__(self, asc=True):
+    def __init__(self):
         """init line asc or desc
 
         Parameters:
@@ -7,8 +7,38 @@ class Line:
 
         """
         self.stack = []
-        self.asc = asc
 
     def stack_on(self, n):
         self.stack.append(n)
+        self.top = n
 
+    def valid_stack(self, n):
+        pass
+
+
+class AscLine(Line):
+    def __init__(self):
+        self.stack = [1]
+        self.top = 1
+
+    def valid_stack(self, n):
+        if n > self.top:
+            return True
+        elif self.top - n == 10:
+            return True
+
+        return False
+
+
+class DescLine(Line):
+    def __init__(self):
+        self.stack = [99]
+        self.top = 99
+
+    def valid_stack(self, n):
+        if n < self.top:
+            return True
+        elif n - self.top == 10:
+            return True
+
+        return False
