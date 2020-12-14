1cfb4534e41521eb0ba75802fc3dce08fbb8a418
ssamko <teacher.ssamko@gmail.com>
2020-12-14 22:23:53 +0900

    prepare for new service

--- theGame.py
+++ theGame.py
@@ -8,6 +8,8 @@ from game import Game
 from player import Player
 
 if __name__ == "__main__":
+    # game.choose_lang()
+    # game.tutorial()
     people = int(input('How many people do you play in? (1~5)'))
     game = Game(people)
     game.shuffle_cards()
